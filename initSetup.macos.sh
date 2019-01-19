da5d112b68e98841b14502ede79d83725a075a2d
Matt Sheehan <sheehamj@outlook.com>
2019-01-19 02:07:15 -0500

    added npmrc

--- initSetup.macos.sh
+++ initSetup.macos.sh
@@ -4,23 +4,25 @@
 
 
 cwd="$(dirname $0)"
-echo $cwd
 
-echo $EUID
-echo $UID
-
-# # add dotfile symlinks
-# ln -s -f "$cwd/bash/.bash_profile" "~/.bash_profile"
-# ln -s -f "$cwd/bash/.bashrc" "~/.bashrc"
-# ln -s -f "$cwd/bash/.bashrc.macos" "~/.bashrc.macos"
-# ln -s -f "$cwd/bash/.aliases" "~/.aliases"
-# ln -s -f "$cwd/.gitconfig" "~/.gitconfig"
-# ln -s -f "$cwd/.ledgerrc" "~/.ledgerrc"
-# ln -s -f "$cwd/.spacemacs" "~/.spacemacs"
-# ln -s -f "$cwd/.tmux.conf" "~/.tmux.conf"
+# echo $cwd
+# echo $EUID
+# echo $UID
 
 
+# add dotfile symlinks
+ln -s -f "$cwd/bash/.bash_profile" "~/.bash_profile"
+ln -s -f "$cwd/bash/.bashrc" "~/.bashrc"
+ln -s -f "$cwd/bash/.bashrc.macos" "~/.bashrc.macos"
+ln -s -f "$cwd/bash/.aliases" "~/.aliases"
+ln -s -f "$cwd/.gitconfig" "~/.gitconfig"
+ln -s -f "$cwd/.ledgerrc" "~/.ledgerrc"
+ln -s -f "$cwd/.spacemacs" "~/.spacemacs"
+ln -s -f "$cwd/.tmux.conf" "~/.tmux.conf"
+ln -s -f "$cwd/.npmrc" "~/.npmrc"
 
-# # brew install archey
+
+# install brew packages
+brew install archey
 
 
