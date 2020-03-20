95f1cd14d573c39c2ddba08f906749204a610436
Matt Sheehan <sheehamj@outlook.com>
2020-03-19 21:06:02 -0400

    add vscode extensions

--- initSetup.macos.sh
+++ initSetup.macos.sh
@@ -3,7 +3,8 @@
 #
 
 
-cwd="$(dirname $0)"
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# cwd="$(dirname $0)"
 
 # echo $cwd
 # echo $EUID
@@ -11,15 +12,17 @@ cwd="$(dirname $0)"
 
 
 # add dotfile symlinks
-ln -s -f "$cwd/bash/.bash_profile" "~/.bash_profile"
-ln -s -f "$cwd/bash/.bashrc" "~/.bashrc"
-ln -s -f "$cwd/bash/.bashrc.macos" "~/.bashrc.macos"
-ln -s -f "$cwd/bash/.aliases" "~/.aliases"
-ln -s -f "$cwd/.gitconfig" "~/.gitconfig"
-ln -s -f "$cwd/.ledgerrc" "~/.ledgerrc"
-ln -s -f "$cwd/.spacemacs" "~/.spacemacs"
-ln -s -f "$cwd/.tmux.conf" "~/.tmux.conf"
-ln -s -f "$cwd/.npmrc" "~/.npmrc"
+ln -s -f "$cwd/bash/.bash_profile" "$HOME/.bash_profile"
+ln -s -f "$cwd/bash/.bashrc" "$HOME/.bashrc"
+ln -s -f "$cwd/bash/.bashrc.macos" "$HOME/.bashrc.macos"
+ln -s -f "$cwd/bash/.aliases" "$HOME/.aliases"
+ln -s -f "$cwd/.gitconfig" "$HOME/.gitconfig"
+ln -s -f "$cwd/.ledgerrc" "$HOME/.ledgerrc"
+ln -s -f "$cwd/.spacemacs" "$HOME/.spacemacs"
+ln -s -f "$cwd/.tmux.conf" "$HOME/.tmux.conf"
+ln -s -f "$cwd/.npmrc" "$HOME/.npmrc"
+ln -s -f "$cwd/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -s -f "$cwd/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
 
 
 # install brew packages
